fdff5390ffc7ed8836a3a49955faf82a25506b0b
shrutiwosti <shrutiwosti@gmail.com>
2023-10-18 14:29:58 +0545

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import Task1 from './component/Task1';
 import Task2 from './component/Task2';
-
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Task1/>
-      <Task2/>
+      <Routes>
+        <Route path="/" element={<Task1 />} />
+        <Route path="customers" element={<Task2 />} />
+      </Routes>
     </div>
   );
 }
+
 
 export default App;
--- src/component/Task1.tsx
+++ src/component/Task1.tsx
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom';
+
 const Task1 = () => {
     return(
         <div>
             <h1>Hello, React!!</h1>
+            <Link to="/customers" style={{color: 'blue', display: 'flex', justifyContent: 'center', alignItems: 'center', }} >Customers Page</Link>
         </div>
     )
 }
